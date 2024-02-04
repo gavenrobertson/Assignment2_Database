ff8d067d64d051de538cde3bbbcf984cc125e8bd
Jorge Alejandre <jorgealejandre.piki@gmail.com>
2024-02-04 00:36:18 -0800

    Added page structure and method to addRecord.

--- classes.h
+++ classes.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <sstream>
 #include <bitset>
+#include <cstring>
+
 using namespace std;
 
 class Record {
@@ -26,31 +28,69 @@ public:
     }
 };
 
+// This can be used to store metadata on the slots used in the slot directory
+struct Slot { 
+    int offset; // Position in page data array
+    int length;
+    bool inUse;
+};
+
+
+struct Page {
+    static const int PAGE_SIZE = 4096;
+    char data[PAGE_SIZE];
+    std::vector<Slot> slots;  // Create a dynamic slot array
+    int freeSpaceOffset;
+
+    // Page constructor that sets up the data array
+    Page() : freeSpaceOffset(0) {
+        memset(data, 0, PAGE_SIZE);
+    }
+
+    bool canFit(int recordSize) const {
+       // Check to see if a given record will fit  in the page block size
+        return PAGE_SIZE - freeSpaceOffset >= recordSize;
+    }
+
+    void addRecord(const vector<char>& record) {
+        int recordSize = record.size(); // Set a record size var from the record
+
+        // If we can not fit the record to the page, throw an error
+        if (!canFit(recordSize)) throw std::runtime_error("Page is full");
+
+        // Check to make sure we don't go over the page size or slot size
+        if (freeSpaceOffset + recordSize > PAGE_SIZE - (slots.size() + 1) * sizeof(Slot)) {
+            throw std::runtime_error("Not enough space for the record and slot metadata");
+        }
+
+        memcpy(data + freeSpaceOffset, record.data(), recordSize);
+
+        // Add a new slot for this record with the given record data
+        slots.push_back(Slot{freeSpaceOffset, recordSize, true});
+
+        // Update the free space offset
+        freeSpaceOffset += recordSize;
+    }
+};
+
+
 //Don't forget to close the files to avoid mem leaks
-
 class StorageBufferManager {
-
 private:
     ofstream employeeRelationFile;
-    const int BLOCK_SIZE = 4096; // initialize the  block size allowed in main memory according to the question
+    const int BLOCK_SIZE = 4096; // initialize the block size allowed in main memory
     int numRecords = 0;
 
+    //  <- Serialize the record info, possibly make a function for this? ->
 
-    // Insert new record 
-    void insertRecord(Record record) {
+   void insertRecord(const Record& record) {
+        // Need to serialize record to make compatible with storing on medium disk
 
-        // No records written yet
-        if (numRecords == 0) {
-            // Initialize first block
+        // Check to see if we need to start a new page or not
 
-        }
-        // Add record to the block
+        // Add the record to the page using .addRecord method somehow
+   }
 
-
-        // Take neccessary steps if capacity is reached (you've utilized all the blocks in main memory)
-
-
-    }
 
 public:
     StorageBufferManager(string NewFileName) : employeeRelationFile(NewFileName, ios::binary){
